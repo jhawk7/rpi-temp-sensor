92d5d6a51dd1f72977a60f5f629102a2e5774eb3
jhawk7 <james.hawkins@careerbuilder.com>
2022-02-26 22:04:37 -0500

    setup healthcheck and read temp loop

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jhawk7/rpi-thermostat/pkg/opentel"
 	log "github.com/sirupsen/logrus"
 	rpio "github.com/stianeikeland/go-rpio"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
-	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel"
+	_ "go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/global"
 )
 
@@ -27,8 +29,15 @@ func main() {
 		log.Fatal(mpErr)
 	}
 
+	tp, tpErr := opentel.InitTraceProvider()
+	if tpErr != nil {
+		log.Fatal(tpErr)
+	}
+
 	//register meterProvider as global mp for package (meterProvider -> meter -> counter)
+	//register traceProvider as global tp for package
 	global.SetMeterProvider(mp)
+	otel.SetTracerProvider(tp)
 
 	//start metric collection
 	ctx := context.Background()
@@ -40,21 +49,37 @@ func main() {
 		if stopErr := mp.Stop(ctx); stopErr != nil {
 			log.Fatal(stopErr)
 		}
+
+		if shutdownErr := tp.Shutdown(ctx); shutdownErr != nil {
+			log.Fatal(shutdownErr)
+		}
 	}()
 
 	//create meter from meter provider (set to global variable)
 	//ds_meter = global.Meter("deathstar_meter")
+	go readTemperature()
 
+	r := gin.New()
+	r.Use(otelgin.Middleware("rpi-thermostat"))
+	r.GET("/healthcheck", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "ok",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
 func readTemperature() {
-	// Temperature sensor input voltage relates to actual temp
-	//Temp in °C = [(Vout in mV) - 500] / 10
-	//((_pin.read()*3.3)-0.500)*100.0;
-	// tempF=(9.0 * myTMP36.read())/5.0 + 32.0;
-	pin.Input()          // Input mode
-	vstate := pin.Read() // Read state from pin (High / Low)
-	read := ((float64(vstate) * 3.3) - 0.5) * 100.0
-	tempF := (9.0*read)/5.0 + 32.0
-	fmt.Sprintf("TempF: %f", tempF)
+	for {
+		// Temperature sensor input voltage relates to actual temp
+		//Temp in °C = [(Vout in mV) - 500] / 10
+		//((_pin.read()*3.3)-0.500)*100.0;
+		// tempF=(9.0 * myTMP36.read())/5.0 + 32.0;
+		pin.Input()          // Input mode
+		vstate := pin.Read() // Read state from pin (High / Low)
+		read := ((float64(vstate) * 3.3) - 0.5) * 100.0
+		tempF := (9.0*read)/5.0 + 32.0
+		fmt.Sprintf("TempF: %f", tempF)
+		time.Sleep(1 * time.Second)
+	}
 }
