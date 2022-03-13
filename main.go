722aede95060279d9fbaf09fa311bc594cbe9ec5
jhawk7 <james.hawkins@careerbuilder.com>
2022-03-13 16:48:27 -0400

    bus line change

--- main.go
+++ main.go
@@ -46,10 +46,10 @@ func readTemperature() {
 		# Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
 	*/
 
-	// Create new connection to I2C bus on line 2 with address 0x44
+	// Create new connection to I2C bus on line 1 with address 0x44
 	// run `i2cdetect -y 1` to view vtable for specific device addr
-	// when loaded a specific i2c folder /dev/i2c-* will be created; using bus 2 for /dev/i2c-2
-	conn, connErr := i2c.NewI2C(0x44, 2)
+	// when loaded, a specific device entry folder /dev/i2c-* will be created; using bus 1 for /dev/i2c-1
+	conn, connErr := i2c.NewI2C(0x44, 1)
 	if connErr != nil {
 		common.ErrorHandler(fmt.Errorf("failed to connect to i2c peripheral device; %v\n", connErr), true)
 	}
