1aed3541bcabbf69fbbf47295cf9696a4d73c9a1
jhawk7 <jhawk@ser>
2024-05-14 16:36:54 -0400

    disconnecting wifi and mqtt during downtime to save power

--- embedded-mp/main.py
+++ embedded-mp/main.py
@@ -42,28 +42,44 @@ class mqttClient:
     msg = json.dumps(obj)
     self.client.publish(self.topic, msg)
     print(f"published values to topic {self.topic}")
+  
+  def disconnect(self):
+    print('disconnecting from mqtt server')
+    self.client.disconnect()
     
 
-def initWifi():
-  print('Connecting to WiFi Network Name:', config.ENV["SSID"])
-  wlan = network.WLAN(network.STA_IF)
-  wlan.active(True) # power up the WiFi chip
-  print('Waiting for wifi chip to power up...')
-  sleep(3) # wait three seconds for the chip to power up and initialize
-  wlan.connect(config.ENV["SSID"], config.ENV["WPASS"])
-  print('Waiting for access point to log us in.')
-  sleep(2)
-  if wlan.isconnected():
-    print('Success! We have connected to your access point!')
-    print('Try to ping the device at', wlan.ifconfig()[0])
-    led.value(False)
-  else:
-    print('Failure! We have not connected to your access point!  Check your config file for errors.')
-    led.value(False)
-    sleep(1)
-    led.value(True)
-    print("reconnecting")
-    return initWifi() #retry
+class wifi:
+  def __init__(self):
+    self.wlan = self.__connectWifi()
+    
+  def __connectWifi(self):
+    print('Connecting to WiFi Network Name:', config.ENV["SSID"])
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True) # power up the WiFi chip
+    print('Waiting for wifi chip to power up...')
+    sleep(3) # wait three seconds for the chip to power up and initialize
+    wlan.connect(config.ENV["SSID"], config.ENV["WPASS"])
+    print('Waiting for access point to log us in.')
+    sleep(2)
+    
+    if wlan.isconnected():
+      print('Success! We have connected to your access point!')
+      print('Try to ping the device at', wlan.ifconfig()[0])
+      led.value(False)
+      return wlan
+    else:
+      print('Failure! We have not connected to your access point!  Check your config file for errors.')
+      led.value(False)
+      sleep(1)
+      led.value(True)
+      print("reconnecting")
+      return self.__connectWifi() #retry
+  
+  def disconnect(self):
+    print('disconnecting from wifi')
+    self.wlan.disconnect()
+    self.wlan.active(False) #power down wlan chip
+
 
 def getReading(i2c):
   i2c.writeto(0x44, bytes([0x2C, 0x06]))
@@ -77,17 +93,22 @@ def getReading(i2c):
   
   
 def main():
-  led.value(True) # LED will be on until wifi is connected successfully
-  initWifi()
-  sleep(2)
-  led.value(True) # LED will remain on until mqtt is connected successfully
-  cMQTT = mqttClient()
   i2c = machine.I2C(1, scl=machine.Pin(15), sda=machine.Pin(14), freq=200_000)
   print(i2c.scan())
+  
   while True:
+    led.value(True) # LED will be on until wifi is connected successfully
+    wconn = wifi()
+    sleep(2)
+    led.value(True) # LED will remain on until mqtt is connected successfully
+    cMQTT = mqttClient()
     temp, humidity = getReading(i2c)
     cMQTT.publish(temp, humidity)
-    sleep(900)
+    sleep(2)
+    print("entering power saver mode..")
+    cMQTT.disconnect()
+    wconn.disconnect()
+    sleep(3600)
 
 
 if __name__ == "__main__":
